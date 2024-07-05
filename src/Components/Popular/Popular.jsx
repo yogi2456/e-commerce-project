11cecd4b5d31e347d42fc664066567069a9cbfc4
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-07-05 22:35:04 +0530

    6th to 9th hours update

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Popular.css"
-import data_product from "../Assets/data"
+// import data_product from "../Assets/data"
 import Item from '../Item/Item'
 
 const Popular = () => {
+
+  const [data_product, setData_product] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/popularinwomen')
+    .then((response) => response.json())
+    .then((data) => setData_product(data))
+  }, [])
+  
   return (
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,18 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./CSS/LoginSignup.css"
 
 const LoginSignup = () => {
+
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
+
+  const login = async () => {
+       console.log("Login Function Executed", formData)
+       let responseData;
+       await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers:{
+          Accept: "application/form-data",
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+       }).then((response) => response.json()).then((data) => responseData=data)
+
+       if(responseData.success){
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace("/");
+       } else {
+        alert(responseData.errors)
+       }
+  }
+
+  const signup = async () => {
+    console.log("Signup Function Executed", formData)
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept: 'application/form-data',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response) => response.json()).then((data) => {responseData=data})
+
+    if(responseData.success)
+      {
+        localStorage.setItem('auth-token',responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.errors)
+      }
+  }
+
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type='email' placeholder='Email Addess'/>
-          <input type="text" placeholder='Password' />
+          {state==="Sign Up"?<input type="text" name='username' value={formData.username} onChange={changeHandler} placeholder='Your Name' />:<></>}
+          <input type='email' name='email' value={formData.email} onChange={changeHandler} placeholder='Email Addess'/>
+          <input type="text" name='password' value={formData.password} onChange={changeHandler} placeholder='Password' />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <button onClick={() => {state === "Login"? login():signup()}}>Continue</button>
+        {state === "Sign Up"
+        ? <p className="loginsignup-login">Already have an account? <span onClick={() => {setState("Login")}}>Login here</span></p>
+        : <p className="loginsignup-login">Create an account <span onClick={() => {setState("Sign Up")}}>Click here</span></p>}
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
